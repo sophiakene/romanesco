6496964ba4217821714ebd6f0e3ba3c15e0c40d6
sophiakene <sophia.conrad@rocketmail.com>
2019-04-25 13:50:55 +0200

    changed RNN cell from SimpleLSTMCell to LSTMCell

--- romanesco/compgraph.py
+++ romanesco/compgraph.py
@@ -19,7 +19,7 @@ def define_computation_graph(vocab_size: int, batch_size: int):
         input_embeddings = tf.nn.embedding_lookup(embedding, inputs)
 
     with tf.name_scope('RNN'):
-        cell = tf.nn.rnn_cell.BasicLSTMCell(C.HIDDEN_SIZE, state_is_tuple=True)
+        cell = tf.nn.rnn_cell.LSTMCell(C.HIDDEN_SIZE, state_is_tuple=True)
         initial_state = cell.zero_state(batch_size, tf.float32)
         rnn_outputs, rnn_states = tf.nn.dynamic_rnn(cell, input_embeddings, initial_state=initial_state)
 
